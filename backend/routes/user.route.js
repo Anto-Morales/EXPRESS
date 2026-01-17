84cd21a33e12cf5a4a5972add613e48a11059f44
Antonio Arellano <arellanoantonio441@gmail.com>
2026-01-17 15:54:29 -0600

    rama Intern

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -4,8 +4,6 @@ const router = Router(); // Crear instancia del router
 // Importar funciones del controlador
 import { registerUser, loginUser, logoutUser} from '../controllers/user.controller.js';
 
-
-
 // ========== Rutas de usuario ==========
 // POST /api/v1/users/register - Registra un nuevo usuario
 router.post("/register", registerUser);
